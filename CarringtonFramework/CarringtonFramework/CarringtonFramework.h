8eb05f0b3079d935a75d4035cd139ed6d18ab3d0
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-06-22 07:19:13 +0200

    Local Framework

---
+++ CarringtonFramework/CarringtonFramework/CarringtonFramework.h
@@ -0,0 +1,18 @@
+//
+//  CarringtonFramework.h
+//  CarringtonFramework
+//
+//  Created by Manyuchi, Carrington C on 2024/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CarringtonFramework.
+FOUNDATION_EXPORT double CarringtonFrameworkVersionNumber;
+
+//! Project version string for CarringtonFramework.
+FOUNDATION_EXPORT const unsigned char CarringtonFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarringtonFramework/PublicHeader.h>
+
+
---
+++ CarringtonFramework/CarringtonFramework/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  CarringtonFramework
+//
+//  Created by Manyuchi, Carrington C on 2024/06/22.
+//
+
+import Foundation
+
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Do something"
+    }
+}
